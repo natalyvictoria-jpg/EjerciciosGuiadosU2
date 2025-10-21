e4ee210b6a20552e59fb8b858cf50e9ebb8367da
naty <1224100674.nvg@gmail.com>
2025-10-21 11:22:24 -0600

    Update MainPila.java

--- MainPila.java
+++ MainPila.java
@@ -12,27 +12,25 @@ public class MainPila {
     public static void main(String[] args) {
         StackArray<String> pila = new StackArray<>(5);
 
+     public class MainPila {
+    public static void main(String[] args) {
+        StackArray<String> pila = new StackArray<>(5);
+
         // Agregar tres nombres
         pila.push("Sofía");
+        pila.push("Paola");
         pila.push("Ramiro");
-        pila.push("Rey");
 
         // Mostrar la cima con peek
-        System.out.println("Elemento en la cima (peek): " + pila.peek());
+   
+        System.out.println(pila.pop());
+        System.out.println(pila.pop());
 
-        // Eliminar un elemento con pop
-        String eliminado = pila.pop();
-        System.out.println("Elemento eliminado (pop): " + eliminado);
 
-        // Mostrar la cima después de pop
-        System.out.println("Elemento en la cima después de pop: " + pila.peek());
-
-        // Agregar otro nombre
-        pila.push("Victoria");
-        System.out.println("Elemento agregado (push): Victoria");
-
-        // Mostrar la cima final
-        System.out.println("Elemento en la cima final: " + pila.peek());
+  
     }
 }
+    }
+}
+
 
