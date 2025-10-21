cc1a0b97ffe906c7fe599ec7f8c75090a9035828
naty <1224100674.nvg@gmail.com>
2025-10-21 11:15:07 -0600

    Update StackArray.java

--- StackArray.java
+++ StackArray.java
@@ -2,8 +2,12 @@
 package pila;
 
 /**
- *
- * @author Nataly Victoria Gonzalez Aviles. GTID141
+ *Este ejercicio fue realizado en clase con el fin de fortalecer mi conocimiento 
+ de manera practica sobre el tema de Pilas. Creamos esta clase con el proposito de 
+ elaborar las operaciones que realizan las pilas y el comportmiento de ella.
+ * @author Nataly Victoria Gonzalez Aviles-GTI0141 
+ *Viernes 17 de Octubre de 2025.
+ Estructura de datos
  */
 public class StackArray <T> implements IStack <T> {
     private T[] elements; // Estructura de datos interna (Array)
@@ -67,5 +71,6 @@ public void push(T elemento) {
     
     
 }
+
 
 
